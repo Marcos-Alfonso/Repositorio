f9ba4319fc22b068981b166554e400a37f603aa1
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-09-16 10:55:59 +0200

    up

--- ADAT/archivos/src/ej3.java
+++ ADAT/archivos/src/ej3.java
@@ -1,8 +1,8 @@
 import java.io.File;
+import java.nio.file.Files;
 
 public class ej3 {
     public static void main(String[] args) {
-        File f = new File(".");
-        System.out.println(f.exists());
+
     }
 }
