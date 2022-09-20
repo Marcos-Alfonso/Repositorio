09094d3659faddc2c68a27d4784730bb2de4904a
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-09-20 12:56:16 +0200

    a

--- ADAT/archivos/src/ej8.java
+++ ADAT/archivos/src/ej8.java
@@ -1,11 +1,12 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
 public class ej8 {
     public static void main(String[] args) {
-        copyPaste("NUEVODIR/2.txt", ".");
+        copyPaste("./NUEVODIR/2.txt", "./");
     }
 
     private static void copyPaste(String s, String d)  {
@@ -13,10 +14,14 @@ public class ej8 {
         File f2 = new File(d);
         if(f1.exists() && f1.isFile() && f2.isDirectory()){
             try {
-                Files.copy(f1.toPath(), f2.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(f1.toPath(),
+                (new File(f2.getPath() + f1.getName())).toPath(),
+                        StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }else{
+
         }
     }
 }
