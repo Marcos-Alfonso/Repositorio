9122f03d15003207c77f03160986baefde515ffb
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-10-03 09:19:08 +0200

    n

--- Interfaces/Ej12/Ej12/Form1.Designer.cs
+++ Interfaces/Ej12/Ej12/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbPalabra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbPalabras = new System.Windows.Forms.ListBox();
+            this.tbStats = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbIgnorar = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tbPalabra
+            // 
+            this.tbPalabra.Location = new System.Drawing.Point(38, 141);
+            this.tbPalabra.Name = "tbPalabra";
+            this.tbPalabra.Size = new System.Drawing.Size(100, 23);
+            this.tbPalabra.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Palabra";
+            // 
+            // lbPalabras
+            // 
+            this.lbPalabras.FormattingEnabled = true;
+            this.lbPalabras.ItemHeight = 15;
+            this.lbPalabras.Location = new System.Drawing.Point(164, 49);
+            this.lbPalabras.Name = "lbPalabras";
+            this.lbPalabras.Size = new System.Drawing.Size(153, 244);
+            this.lbPalabras.Sorted = true;
+            this.lbPalabras.TabIndex = 2;
+            // 
+            // tbStats
+            // 
+            this.tbStats.Location = new System.Drawing.Point(342, 102);
+            this.tbStats.Multiline = true;
+            this.tbStats.Name = "tbStats";
+            this.tbStats.ReadOnly = true;
+            this.tbStats.Size = new System.Drawing.Size(160, 147);
+            this.tbStats.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(390, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Estadísticas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbIgnorar
+            // 
+            this.cbIgnorar.AutoSize = true;
+            this.cbIgnorar.Location = new System.Drawing.Point(52, 199);
+            this.cbIgnorar.Name = "cbIgnorar";
+            this.cbIgnorar.Size = new System.Drawing.Size(64, 19);
+            this.cbIgnorar.TabIndex = 6;
+            this.cbIgnorar.Text = "Ignorar";
+            this.cbIgnorar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(514, 312);
+            this.Controls.Add(this.cbIgnorar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbStats);
+            this.Controls.Add(this.lbPalabras);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPalabra);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox tbPalabra;
+        private Label label1;
+        private ListBox lbPalabras;
+        private TextBox tbStats;
+        private Label label2;
+        private Button button1;
+        private CheckBox cbIgnorar;
     }
 }
--- Interfaces/Ej12/Ej12/Form1.cs
+++ Interfaces/Ej12/Ej12/Form1.cs
@@ -6,5 +6,56 @@ namespace Ej12
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(tbPalabra.Text != "")
+            {
+                if (cbIgnorar.Checked)
+                {
+                    introducePalabra(tbPalabra.Text);
+                }
+                else
+                {
+                    if (!lbPalabras.Items.Contains(tbPalabra.Text))
+                    {
+                        introducePalabra(tbPalabra.Text);
+                    }
+                }
+            }
+        }
+
+        private void introducePalabra(string text)
+        {
+           lbPalabras.Items.Add(text);
+            tbPalabra.Text = "";
+            tbStats.Text = "-Número de palabras: " + lbPalabras.Items.Count
+                + "\r\n-Número de carácteres totales: " + charCount()
+                + "\r\n-Número de espacios : " + spaceCount();
+        }
+
+        private string charCount()
+        {
+            int count = 0;
+            for (int i = 0; i < lbPalabras.Items.Count; i++)
+            {
+                count +=lbPalabras.Items[i].ToString().Length;
+            }
+            return count.ToString();
+        }
+        private string spaceCount()
+        {
+            String s;
+            int count = 0;
+            for (int i = 0; i < lbPalabras.Items.Count; i++)
+            {
+                s=lbPalabras.Items[i].ToString();
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[j] == ' ') count++;
+                }
+            }
+            return count.ToString();
+        }
     }
 }
