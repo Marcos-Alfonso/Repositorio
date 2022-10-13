439c19a722e994215546e210743a7b2d9b489642
MarcKAlf <malfgar2109@g.educaand.es>
2022-10-13 22:37:25 +0200

    Revert "Delete Ej3.php"
    This reverts commit 3c15746a1a9127a2ea267d1d0f8182323cd66dbe.

---
+++ PHP/Ej3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//3. Crea un array multidimensional de evaluación de Alumnos de 1º de DAW. En cadafila debe aparecer: Nombre del Alumno, Nota 1º Eval, Nota 2º Eval y Nota final
+//• La clase consta de diez alumnos
+//• La nota final obedece a la siguiente fórmula: Nota final = 0,4 * Nota 1ª Eval +0,6* Nota 2ª Eval
+//• Carga el array con datos ficticios. Las calificaciones se harán en pasos de 0.5
+//• En el caso de que la nota final sea de 4.5 se redondeará a 5
+//• Por un despiste hay que introducir los datos de Carlos Ruiz Rus, 4, 4; Marcos Pérez López, 4.5, 5; y de Josefa Portillo Ruiz, 3, 5
+//• Nuevo error: la nota de la 1ª Evaluación de Josefa Portillo es 4
+//• Hay que dar de baja a Carlos Ruiz Rus, no ha pagado la matrícula
+//• Produce un listado ordenado de mayor a menor según la Nota final
+//• Especifica por pantalla los alumnos que obtuvieron mejores calificaciones en la primera y segunda evaluación
+/*• Presenta dicha lista en una tabla HTML con las cabeceras Nombre del Alumno,
+Nota 1º Eval, Nota 2º Eval y Nota final. Dibuja la cabecera en color Azul Claro y
+las columnas pares en tono Gris. Debajo de la columna Nota Final incluye la nota
+media del Curso
+*/
+?>
+
+</body>
+</html>
