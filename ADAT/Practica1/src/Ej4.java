61ebd427f779a6ea15e98ef22c82245795aa667e
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-10-26 10:30:02 +0200

    n

--- ADAT/Practica1/src/Ej4.java
+++ ADAT/Practica1/src/Ej4.java
@@ -2,36 +2,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
 
 public class Ej4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         try {
-            URL url = new URL("http://www.expansion.com/mercados/cotizaciones/indices/ibex35_I.IB.html?ci%d=SEM23201");
+            URL url = new URL("https://www.expansion.com/mercados/cotizaciones/indices/ibex35_I.IB.html");
             URLConnection conexion = url.openConnection();
             conexion.connect();
 
-            //Lectura
             InputStream is = conexion.getInputStream();
 
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             char[] buffer = new char[1000];
 
             //ACA EMPIEZA MI CONDICIÓN DESDE ACÁ PODES CAMBIAR VOS
-            int leido;
+            String leido;
             String texto = "";
-            while ((leido = br.read(buffer)) > 0) {
-                String datos = new String(buffer, 0, leido);
-                texto += datos;
+            boolean read = false;
+            while ((leido = br.readLine()) != null) {
+                    texto+=leido;
+                    texto+=System.lineSeparator();
             }
-            System.out.println(texto);
+            String [] rows = getRow(texto);
+            String [] lines = getLineas(rows);
 
-        }catch (IOException ex){
+        }catch (Exception ex){
             ex.printStackTrace();
         }
 
+
+    }
+
+    private static String[] getLineas(String[] rows) {
+        for(String s: rows){
+
+            System.out.println("----------------------------");
+            for(String st: s.split("td")){
+                if(!st.contains("primera")){
+                    String x = st.replaceAll(".*\\>|\\<.*", "");
+                    //System.out.println(x);
+                    st=x;
+                }
+                System.out.println(st);
+            }
+                /*
+                if(!s.contains("primera")){
+                    String x = s.replaceAll(".*\\>|\\<.*", "");
+                    //System.out.println(x);
+                    s=x;
+                }
+                System.out.println(s);
+                */
+        }
+        return null;
+    }
+
+    private static String[] getRow(String texto) {
+        String []tablas =texto.split("tbody");
+        for(String t:tablas){
+            if(t.contains("/mercados/cotizaciones/valores/acciona_M.ANA.html"))
+                return t.split("tr");
+        }
+        return null;
     }
 }
