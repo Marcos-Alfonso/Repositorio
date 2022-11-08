4a9c1e342795bb33286de41c197081b1f93c19ca
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-11-08 14:18:47 +0100

    n

--- ADAT/Practica2XML/src/Ej5_1.java
+++ ADAT/Practica2XML/src/Ej5_1.java
@@ -1,30 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.util.ArrayList;
+
 
 public class Ej5_1 {
+    static JComboBox<String> combo = new JComboBox<>();
+    static ArrayList<ArrayList<String>> lista = new ArrayList<>();
+    static JTextArea area = new JTextArea();
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Mi primera GUI");
+        JFrame frame = new JFrame("Carga xml");
+        combo.addActionListener(e -> {
+
+        });
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BorderLayout bl = new BorderLayout();
         frame.setLayout(bl);
-        frame.setSize(300, 300);
+        frame.setSize(500, 500);
 
-        JButton b = new JButton("Presionar");
-        frame.getContentPane().add(b, BorderLayout.CENTER);
+        JButton b = new JButton("Importar");
+        frame.getContentPane().add(area, BorderLayout.CENTER);
+        frame.getContentPane().add(b, BorderLayout.SOUTH);
+        frame.getContentPane().add(combo, BorderLayout.NORTH);
+
         b.addActionListener(e -> {
             JFileChooser chooser = new JFileChooser();
             if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
                 File f = chooser.getSelectedFile();
-
-            } else {
-
+                cargaXML(f);
             }
         });
-
         frame.setVisible(true);
     }
 
+    private static void cargaXML(File f) {
+
+    }
+/*
     public void selectFile() {
         JFileChooser chooser = new JFileChooser();
         // optionally set chooser options ...
@@ -35,6 +47,5 @@ public class Ej5_1 {
             // user changed their mind
         }
     }
-
-
+    */
 }
