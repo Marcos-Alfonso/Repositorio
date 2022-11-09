28d97267963c6a48ec9e04c33c1f64d0800a988c
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-11-09 10:54:24 +0100

    n

--- ADAT/Practica2XML/src/Ej5_1.java
+++ ADAT/Practica2XML/src/Ej5_1.java
@@ -1,4 +1,11 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
@@ -11,7 +18,7 @@ public class Ej5_1 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Carga xml");
         combo.addActionListener(e -> {
-
+            cargaTX();
         });
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BorderLayout bl = new BorderLayout();
@@ -32,8 +39,60 @@ public class Ej5_1 {
         });
         frame.setVisible(true);
     }
+    private static void cargaTX(){
+        int i = combo.getSelectedIndex();
+        ArrayList<String> pelicula=lista.get(i);
+        String tx = "";
+        for (String s:pelicula) {
+            tx +=s+"\n";
+        }
+        area.setText(tx);
+    }
 
-    private static void cargaXML(File f) {
+    private static void cargaXML(File f) {{
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try{
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(f);
+            document.getDocumentElement().normalize();
+
+            //crea una lista con todos los nodos pelicula
+            NodeList peliculas = document.getElementsByTagName("Pelicula");
+            NodeList actores = document.getElementsByTagName("Actores");
+
+            for (int i = 0; i < peliculas.getLength(); i ++) {
+                ArrayList<String> array = new ArrayList<>();
+                //obtener un nodo
+                Node peli = peliculas.item(i);
+                //tipo de nodo
+                if (peli.getNodeType() == Node.ELEMENT_NODE) {
+                    //obtener los elementos del nodo
+                    Element e = (Element) peli;
+                    array.add("Titulo: "+e.getElementsByTagName("Titulo").item(0).getTextContent());
+                    array.add("Duración: "+e.getElementsByTagName("Duracion").item(0).getTextContent());
+                    array.add("Género: "+e.getElementsByTagName("Genero").item(0).getTextContent());
+                    array.add("Sinopsis: "+e.getElementsByTagName("sinopsis").item(0).getTextContent());
+                    array.add("Título: "+e.getElementsByTagName("Fecha").item(0).getTextContent());
+                    array.add("Director: "+e.getElementsByTagName("Director").item(0).getTextContent());
+
+                    String ac = "Actores: \n";
+                    NodeList n = (NodeList) actores.item(i);
+                    for (int j = 0; j < n.getLength(); j++) {
+                        Node c = n.item(j);
+                        if (c.getNodeType() == Node.ELEMENT_NODE) {
+                            ac += "\t"+n.item(j).getTextContent()+"\n";
+                        }
+                    }
+                    array.add(ac);
+                    lista.add(array);
+                    combo.addItem(array.get(0));
+                }
+            }
+            cargaTX();
+        } catch (Exception e)
+        {e.printStackTrace();}
+    }
 
     }
 /*
