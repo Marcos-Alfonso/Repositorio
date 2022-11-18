a537f276de3e46e3aa88674c636c2bbff9c8180d
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-11-18 13:34:42 +0100

    n

---
+++ Interfaces/Biblioteca/textNumberLib/textNumberLib/txNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textNumberLib
+{
+    public partial class txNumber: TextBox
+    {
+        public txNumber()
+        {
+            InitializeComponent();
+        }
+        private void change(object sender, EventArgs e)
+        {
+
+            TextBox t = (TextBox)sender;
+                if (System.Text.RegularExpressions.Regex.IsMatch(t.Text, "[^0-9]"))
+            {
+                t.Text = t.Text.Remove(t.Text.Length - 1);
+            }
+        }
+    }
+}
