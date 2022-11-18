9168247cef1ee0a06e3ac8b3f82d374978d8fbc7
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-11-18 10:37:51 +0100

    n

---
+++ ADAT/BBDD/Ej2_6/src/Ej2_8.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+import java.util.Locale;
+
+public class Ej2_8 {
+    public static void main(String[] args) {
+        try {
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba", "root", "root");
+            DatabaseMetaData dbmd = c.getMetaData();
+            System.out.println("Información Base de Datos\n" +
+                    "------------------------------------");
+            System.out.printf("Nombre: %s %n", dbmd.getDatabaseProductName());
+            System.out.printf("Driver: %s %n", dbmd.getDriverName());
+            System.out.printf("URL: %s %n", dbmd.getURL());
+            System.out.printf("Usuario: %s %n", dbmd.getUserName());
+            ResultSet rs = dbmd.getTables("prueba", null, null, null);
+            while (rs.next()){
+                System.out.printf("Tabla %s %n", rs.getString("TABLE_NAME").toUpperCase());
+                System.out.printf("\tCatalogo: %s %n",rs.getString("TABLE_CAT"));
+                System.out.printf("\tEsquema: %s %n", rs.getString("TABLE_SCHEM"));
+                System.out.printf("\tTipo: %s %n", rs.getString("TABLE_TYPE"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
--- ADAT/BBDD/Ej2_6/src/Main.java
+++ ADAT/BBDD/Ej2_6/src/Main.java
@@ -1,5 +1,36 @@
+import java.sql.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        try {
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba", "root", "root");
+            Statement stmt=c.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from empleados");
+            /*
+            rs.last();
+            System.out.println("Nº Filas"+rs.getRow());
+            rs.beforeFirst();
+             */
+            while (rs.next()){
+                System.out.printf("Fila %d: %s, %s, %f\n",
+                        rs.getRow(),
+                        rs.getString("apellido"),
+                        rs.getString("oficio"),
+                        rs.getFloat("salario"));
+            }
+            rs = stmt.executeQuery("SELECT apellido, salario, dnombre FROM empleados NATURAL JOIN departamentos \n" +
+                    "WHERE salario = (SELECT MAX(salario) FROM empleados)");
+            rs.next();
+            System.out.printf("Empleado con mayor salario:\n %s, %f, %s ",
+                    rs.getString("apellido"),
+                    rs.getFloat("salario"),
+                    rs.getString("dnombre"));
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
