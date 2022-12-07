86fad8dead886b15a00a3053a7fa78c0466f5b20
Marcos-Alfonso <malfgar2109@g.educaand.es>
2022-12-07 11:57:30 +0100

    n

---
+++ Interfaces/Ahorca2/Ahorcado/Form2.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Text.Json;
+using static System.Windows.Forms.LinkLabel;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using System.IO;
+using System.Drawing.Text;
+
+namespace Ahorcado
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile("../../../Resources/" + "/Minecraft.ttf");
+            foreach(Control c in this.Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Font = new Font(pfc.Families[0], b.Font.Size, b.Font.Style);
+                }
+                catch (Exception ex){ }
+                try
+                {
+                    TextBox tb = (TextBox)c;
+                    tb.Font = new Font(pfc.Families[0], tb.Font.Size, tb.Font.Style);
+                }
+                catch (Exception ex) { }
+            }
+
+        }
+        List<PictureBox> lista = new List<PictureBox>();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            lista.AddRange(new Collection<PictureBox> {sq6, sq5, sq4,sq3,sq2,sq1 });
+            start = System.DateTime.Now;
+            foreach (PictureBox p in lista)
+            {
+                p.Visible = true;
+            }/*
+            sq1.Visible = false;
+            sq2.Visible = false;
+            sq3.Visible = false;
+            sq4.Visible = false;
+            sq5.Visible = false;
+            sq6.Visible = false;
+            */
+        }
+
+        String palabra;
+        String categoria;
+        String nombre;
+        int puntos = 0;
+        DateTime start;
+
+        public void init(String nombre, String categoria, String palabra) {
+            txUserName.Text = "Usuario: " + nombre; 
+            this.nombre = nombre;
+
+            txCategoria.Text = "Categoría: "+categoria;
+            this.categoria = categoria;
+
+            //txPalabra.Text = getCodified(palabra) ;
+           
+            txPalabra.Text = getCodified(palabra);
+            this.palabra = palabra;
+
+            txPuntos.Text = "Puntos: "+puntos;
+        }
+
+        private string getCodified(string p)
+        {
+            String s = "";
+            foreach (char c in p)
+            {
+                s += "_ ";
+            }
+            return s; 
+
+        }
+        int nErrores = 0;
+        private void buttonClick(object sender, EventArgs e)
+        {
+            
+            Button b = (Button)sender;
+            if (palabra.ToLower().Contains(b.Text))
+            {
+                puntos+=2;
+                char c = b.Text.ToCharArray()[0];
+                recodify(c);
+                b.BackColor = Color.Green;
+                if (!txPalabra.Text.Contains("_")) {
+                    MessageBox.Show("Correcto! La palabra era: "+palabra, "Has ganado!");
+                    puntos += 10;
+                    launchEnding();
+                }
+            }
+            else{
+                puntos --;
+                b.BackColor = Color.Red;
+                txErrorChars.Text += b.Text + "/";
+                txErrorCount.Text = "Número de errores: "+(++nErrores)+"/6";
+                foreach (PictureBox p in lista)
+                {
+                    if (p.Visible)
+                    {
+                        p.Visible = false;
+                        break;
+                    }
+                }
+                if (nErrores >= 6)
+                {
+                    puntos -= 5;
+                    MessageBox.Show("Número de errores máximos alcanzado. La palabra era: "+palabra, "F");
+                    
+                    launchEnding();
+                    
+                }
+            }
+            b.Enabled = false;
+            
+           
+        }
+
+        private void launchEnding()
+        {
+            Persona p = new Persona();
+            p.nombre = nombre;
+            p.puntos = puntos;
+            p.gameTime = System.DateTime.Now - start;
+           
+            List<Persona> parts = loadArray();
+
+            bool tiene = false;
+            foreach (Persona part in parts)
+            {
+                if(part.nombre == p.nombre)
+                {
+                    part.puntos += p.puntos;
+                    part.gameTime += System.DateTime.Now - start;
+                    tiene = true;
+                }
+            }
+            if(!tiene)parts.Add(p);
+            System.Xml.Serialization.XmlSerializer writer =
+            new System.Xml.Serialization.XmlSerializer(typeof(List<Persona>));
+
+            //var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//leaderBoards.xml";
+            String path = "../../../Resources/" + "//leaderBoards.xml";
+
+            System.IO.FileStream file = System.IO.File.Create(path);
+            writer.Serialize(file, parts);
+            file.Close();
+
+            
+            Form3 f = new Form3();
+            f.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            f.init(nombre, puntos);
+            this.Hide();
+            f.Closed += (s, args) => this.Close();
+            //f.ShowDialog();
+            f.Show();
+            
+        }
+
+        public static  List<Persona> loadArray()
+        {
+            try
+            {
+                String path = "../../../Resources/" + "//leaderBoards.xml";
+                var serializer = new XmlSerializer(typeof(List<Persona>));
+                using (var reader = XmlReader.Create(path))
+                {
+                    var lista = (List<Persona>)serializer.Deserialize(reader);
+                    reader.Close();
+                    return lista;
+                }
+                
+            }catch(Exception ex)
+            {
+                
+                return new List<Persona>();
+            }
+            
+        }
+
+        private void recodify(char c)
+        {
+            c = Char.ToUpper(c);
+            for(int i = 0; i<palabra.Length; i++)
+            {
+                if(palabra.ToUpper().ToCharArray()[i] == c)
+                {
+                    StringBuilder sb = new StringBuilder(txPalabra.Text);
+                    sb[i * 2] = c;
+                    txPalabra.Text = sb.ToString();
+                }
+            }
+            
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+            String s = (System.DateTime.Now - start).ToString();
+            txPlaytime.Text = s.Substring(0,8);
+            txPuntos.Text = "Puntos: " + (puntos);
+            //label1.Text = System.DateTime.Now.ToString().Substring(10);
+        }
+
+        private void surrender(object sender, EventArgs e)
+        {
+            puntos -= 5;
+            MessageBox.Show("La palabra era: "+palabra, "Rendición");
+            
+            launchEnding();
+        }
+    }
+
+    [Serializable]
+    [XmlRoot("Persona")]
+    public partial class Persona
+    {
+        public string nombre { get; set; }
+        public int puntos { get; set; }
+
+        public TimeSpan gameTime { get; set; }
+    }
+}
---
+++ Interfaces/Ahorca2/Ahorcado/Form3.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Ahorcado
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Puntos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tiempo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tx
+            // 
+            this.tx.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tx.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.tx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tx.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tx.ForeColor = System.Drawing.SystemColors.Menu;
+            this.tx.Location = new System.Drawing.Point(28, 12);
+            this.tx.Multiline = true;
+            this.tx.Name = "tx";
+            this.tx.ReadOnly = true;
+            this.tx.Size = new System.Drawing.Size(395, 23);
+            this.tx.TabIndex = 33;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Puntos,
+            this.Tiempo});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(289, 165);
+            this.dataGridView1.TabIndex = 34;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 76;
+            // 
+            // Puntos
+            // 
+            this.Puntos.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Puntos.HeaderText = "Puntuación";
+            this.Puntos.Name = "Puntos";
+            this.Puntos.ReadOnly = true;
+            this.Puntos.Width = 93;
+            // 
+            // Tiempo
+            // 
+            this.Tiempo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Tiempo.HeaderText = "Tiempo";
+            this.Tiempo.Name = "Tiempo";
+            this.Tiempo.ReadOnly = true;
+            this.Tiempo.Width = 73;
+            // 
+            // btBack
+            // 
+            this.btBack.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btBack.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btBack.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btBack.Location = new System.Drawing.Point(339, 156);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(84, 50);
+            this.btBack.TabIndex = 35;
+            this.btBack.Text = "Menu Principal";
+            this.btBack.UseVisualStyleBackColor = false;
+            this.btBack.Click += new System.EventHandler(this.button26_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(435, 234);
+            this.Controls.Add(this.btBack);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tx);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tx;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Puntos;
+        private DataGridViewTextBoxColumn Tiempo;
+        private Button btBack;
+    }
+}
