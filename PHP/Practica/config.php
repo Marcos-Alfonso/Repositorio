bcebf9a993451be56771f845ecd6ab6af10deb22
Marcos-Alfonso <malfgar2109@g.educaand.es>
2023-01-30 13:34:50 +0100

    r

--- PHP/Practica/config.php
+++ PHP/Practica/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'localhost:3307');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'practicaPHP');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'test');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
