ca92fb84bdd11ec433f9a583c8a0528f88ffacf3
Marcos-Alfonso <malfgar2109@g.educaand.es>
2023-02-07 10:11:35 +0100

    m

--- Android/app/src/main/java/com/example/reproductor/DB.java
+++ Android/app/src/main/java/com/example/reproductor/DB.java
@@ -1,6 +1,7 @@
 package com.example.reproductor;
 
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.example.reproductor.modelos.Usuario;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -19,24 +22,37 @@ import java.util.ArrayList;
 public class DB extends SQLiteOpenHelper {
 
     public static final int DB_VERSION = 1;
-    public static final String DB_NOMBRE = "DB_REPRODUCTOR";
-    public static final String TABLA_USUARIOS = "CREATE TABLE USUARIOS( id INTEGER PRIMARY KEY AUTOINCREMENT ,nombre TEXT NOT NULL,imagen BLOB default 10,fecha String default null)";
-
+    public static final String DB_NOMBRE = "reproductor.db";
+    public static final String TABLA_USUARIOS = "CREATE TABLE USUARIOS( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre String NOT NULL, fecha String default null, imagen String default \"\", genero String default null)";
+    public static final String DELETE_USUARIOS = "DROP TABLE IF EXISTS USUARIOS";
     public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DB_NOMBRE, null, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
         sqLiteDatabase.execSQL(TABLA_USUARIOS);
-
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
-
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(DELETE_USUARIOS);
+        onCreate(db);
     }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+    public long addUsuario(String nombre, String fecha, String imagen, String genero){
+        SQLiteDatabase sqldb = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nombre", nombre);
+        values.put("fecha", fecha);
+        values.put("imagen", imagen);
+        values.put("genero", genero);
 
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = sqldb.insert("usuarios", null, values);
+        return newRowId;
+    }
 }
---
+++ Android/app/src/main/java/com/example/reproductor/modelos/Usuario.java
@@ -0,0 +1,12 @@
+package com.example.reproductor.modelos;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String fecha;
+    private String imgSrc;
+    private String genero;
+    public Usuario(){
+
+    }
+}
