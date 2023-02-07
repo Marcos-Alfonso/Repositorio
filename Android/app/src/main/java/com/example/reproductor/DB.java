767d0a92e9aad304a943db2bfa6f1be76e5c0cd3
MarcKAlf <malfgar2109@g.educaand.es>
2023-02-07 23:41:28 +0100

    a

--- Android/app/src/main/java/com/example/reproductor/DB.java
+++ Android/app/src/main/java/com/example/reproductor/DB.java
@@ -23,21 +23,36 @@ public class DB extends SQLiteOpenHelper {
 
     public static final int DB_VERSION = 1;
     public static final String DB_NOMBRE = "reproductor.db";
-    public static final String TABLA_USUARIOS = "CREATE TABLE USUARIOS( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre String NOT NULL, fecha String default null, imagen String default \"\", genero String default null)";
-    public static final String DELETE_USUARIOS = "DROP TABLE IF EXISTS USUARIOS";
+    //public static final String TABLA_USUARIOS = "CREATE TABLE USUARIOS( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre String NOT NULL, fecha String default null, imagen String default \"\", genero String default null)";
+   // public static final String DELETE_USUARIOS = "DROP TABLE IF EXISTS USUARIOS";
+    public static final String[] TABLAS = new String[]{
+            "CREATE TABLE USUARIOS( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre String NOT NULL, fecha String default null, imagen String default \"\", genero String default null)",
+            "CREATE TABLE PLAYLIST( id INTEGER PRIMARY KEY AUTOINCREMENT, nombre String NOT NULL, imagen String default \"\", id_usuario INTEGER NOT NULL)",
+            "CREATE TABLE CANCIONES( id_playlist INTEGER PRIMARY KEY, src String NOT NULL)"
+    };
+    public static final String[] DELETE_TABLAS = new String[]{
+            "DROP TABLE IF EXISTS USUARIOS",
+            "DROP TABLE IF EXISTS PLAYLIST",
+            "DROP TABLE IF EXISTS CANCIONES"
+    };
     public DB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DB_NOMBRE, null, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(TABLA_USUARIOS);
+        for (String query:TABLAS) {
+            sqLiteDatabase.execSQL(query);
+        }
+
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-        db.execSQL(DELETE_USUARIOS);
+        for (String query:DELETE_TABLAS) {
+            db.execSQL(query);
+        }
         onCreate(db);
     }
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -54,5 +69,27 @@ public class DB extends SQLiteOpenHelper {
         // Insert the new row, returning the primary key value of the new row
         long newRowId = sqldb.insert("usuarios", null, values);
         return newRowId;
+    }
+    public int userExist(String nombre){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {
+                "id"
+        };
+        String selection = "nombre = ?";
+        String[] selectionArgs = { nombre };
+        Cursor cursor = db.query(
+                "USUARIOS",   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                null               // The sort order
+        );
+        while(cursor.moveToNext()) {
+            int itemId = cursor.getInt(0);
+            return itemId;
+        }
+        return -1;
     }
 }
